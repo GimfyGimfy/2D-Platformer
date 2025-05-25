5d4212bbb7fa338df4e027907add051adad04018
GimfyGimfy <78263704+GimfyGimfy@users.noreply.github.com>
2025-05-25 21:04:10 +0200

    Delete SPEEDOS directory
    Not needed anymore

--- SPEEDOS/entities/platforms.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from entities.game_object import GameObject
-from constants import COLORS
-
-class Platform(GameObject):
-    def __init__(self, x: int, y: int):
-        super().__init__(x, y)
-        self.image.fill(COLORS["PLATFORM"])
